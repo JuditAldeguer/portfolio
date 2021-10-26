2645e7819224441b954c0c9653d25f0ff6802af7
JuditAldeguer <jud.vicens@hotmail.es>
2021-10-26 20:38:57 +0200

    add script insignia LinkedIn

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,33 @@ import Links from './secondary-components/Links';
 
 const Header = (props) => {
   return (
-    <header className="header">
-      <section id="header" className="header">
+    <header id="header" className="header">
+      <section className="header">
         <div className="cover">
           <Links />
         </div>
         <div className="coverText">
-          <h1 className="cover_mainTitle">Judit Aldeguer Vicens</h1>
-          <h2 className="cover_title">Junior Frontend Developer</h2>
-          <p className="cover_text">Get to know more about me!</p>
+          <div>
+            <h1 className="cover_mainTitle">Judit Aldeguer Vicens</h1>
+            <h2 className="cover_title">Junior Frontend Developer</h2>
+            <p className="cover_text">Get to know more about me!</p>
+          </div>
+          <div
+            className="badge-base LI-profile-badge linkedin_insignia"
+            data-locale="es_ES"
+            data-size="medium"
+            data-theme="light"
+            data-type="VERTICAL"
+            data-vanity="juditaldeguer"
+            data-version="v1"
+          >
+            <a
+              className="badge-base__link LI-simple-link"
+              href="https://es.linkedin.com/in/juditaldeguer?trk=profile-badge"
+            >
+              _
+            </a>
+          </div>
         </div>
       </section>
     </header>

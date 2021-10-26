9abf34fcf3da8586b80befdfe89d1fc83697625f
JuditAldeguer <jud.vicens@hotmail.es>
2021-10-26 18:33:01 +0200

    update links

--- src/components/App.js
+++ src/components/App.js
@@ -13,16 +13,42 @@ import NotFoundPage from './secondary-components/NotFoundPage';
 function App() {
   return (
     <div className="page">
+      {/* <Header />
+      <Main />
+      <Footer /> */}
       <Switch>
         <Route path="/" exact>
           <Header />
           <Main />
           <Footer />
         </Route>
-        <Route path="/CV/">
+        <Route path="/aboutMe" exact>
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+        <Route path="/projects" exact>
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+        <Route path="/contact" exact>
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+        <Route path="/header" exact>
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+        <Route path="/CV" exact>
           <p>Descargar curriculum</p>
         </Route>
         <Route>
+          <Header />
+          <Main />
+          <Footer />
           <NotFoundPage />
         </Route>
       </Switch>

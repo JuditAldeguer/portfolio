76c9ed0a406342531e7bf748b1beb2ebb0ae0c3a
JuditAldeguer <jud.vicens@hotmail.es>
2021-10-26 14:17:03 +0200

    update structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
-import { useEffect, useState } from 'react';
-import { v4 as uuid } from 'uuid'; // al generar id={uuid()}
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 //Services
 import callToApi from '../services/api';
-import ls from '../services/local-storage.js'; //localStorage
-import date from '../services/date'; //fecha usamos date: date.getCurrentDate()
 //Styles
 import '../styles/App.scss';
 //Components

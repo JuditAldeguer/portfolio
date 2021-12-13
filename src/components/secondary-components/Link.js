194f71ff956787f63b20a3796099d32861c91f77
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-13 19:58:15 +0100

    update HashLink

--- src/components/secondary-components/Link.js
+++ src/components/secondary-components/Link.js
@@ -1,12 +1,14 @@
 //i18
 import { useTranslation } from 'react-i18next';
+//HashLink
+import { HashLink } from  'react-router-hash-link'; 
 
 const Link = (props) => {
   const [t, i18n] = useTranslation('nav');
   return (
-    <a className="nav_link" href={`#${props.link}`} title={props.link}>
-      {t(`nav.${props.children}`)}
-    </a>
+    <HashLink  className="nav_link" to={`#${props.link}`} title={props.link}>
+    {t(`nav.${props.children}`)}
+  </HashLink>
   );
 };
 

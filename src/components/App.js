44651d0ca97b2ecbc480072dcb8b4224ccfdc4b3
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-13 20:01:58 +0100

    test

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ function App() {
               target="_blank"
               rel="noreferrer"
             >
-              Download English CV
+              Download English CV asdsadf
             </a>
             <a
               className="link_download"
